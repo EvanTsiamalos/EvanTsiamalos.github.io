7bf0ea436c0657ed4882ce58afcc6ec06ed3e300
EvanTsiamalos <75636638+EvanTsiamalos@users.noreply.github.com>
2023-05-27 00:47:26 -0500

    update tester

--- material/tester.py
+++ material/tester.py
@@ -3,4 +3,7 @@ class Tester:
     def tesstCase1():
         assert "variable_1" in globals(), "you should name the variable as variable_1"
         assert globals()["variable_1"] == 6**7, "your_calculation does not seem correct"
-        
+    
+    
+    def testCase2():
+        assert variable_1 == 6**7, "your_calculation does not seem correct"
