1bfc45adfa2e2896cec7374160315fa7246b0509
EvanTsiamalos <75636638+EvanTsiamalos@users.noreply.github.com>
2023-06-22 16:30:33 -0500

    correction

--- material/assignment_1/assignment1_tester.py
+++ material/assignment_1/assignment1_tester.py
@@ -2,19 +2,18 @@ class Tester:
 
     def tesstCase1():
         assert "variable_1" in globals(), "You should have a variable called 'variable_1'."
-        assert globals()["variable_1"] == 6**7, "Your calculation for variable does not seem correct"
+        assert globals()["variable_1"] == 6**7, "Your calculation for variable does not seem correct. Try again."
     
     
     def testCase2():
         assert "df2" in globals(), "You should have a variable called 'df2'"
-        #assert globals()["df2"] == 6**7, "your calculation does not seem correct"
         assert df2 == df["Major"], "There seems to be a problem with df2. Please try again." 
       
       
-      def testCase2():
-        assert "df3" in globals(), "You should have a variable called 'df2'"
+    def testCase3():
+        assert "df3" in globals(), "You should have a variable called 'df3'"
         
-        assert df3 == df["Major"], "There seems to be a problem with df2. Please try again." 
+        assert (("Major" in df3.columns) and ("Admission" in df3.columns) and ("Gender" in df3.columns)), "There seems to be a problem with df3. Please try again." 
       
       
       
